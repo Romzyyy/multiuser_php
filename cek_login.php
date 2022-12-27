350b3c54754d8fb04df37a4c14d599e3d289684e
romzyyy <fijay0117@gmail.com>
2022-12-28 03:17:36 +0700

    remove for check num of variable login

--- cek_login.php
+++ cek_login.php
@@ -4,9 +4,7 @@ include 'koneksi.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 $login = mysqli_query($koneksi,"select * from user where username='$username' and password='$password'");
-$cek = mysqli_num_rows($login);
  
-if($cek > 0){
 	$data = mysqli_fetch_assoc($login);
 	if($data['level']=="siswa"){
 		$_SESSION['username'] = $username;
@@ -23,8 +21,5 @@ if($cek > 0){
 	}else{
 	header("location:index.php?pesan=gagal");
 	}	
-}else{
-	header("location:index.php?pesan=gagal");
-}
  
 ?>
